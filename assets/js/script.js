46e53e63e27325ae7a61a558dd513eee5ec684fa
JulianeGampe <juliane.gampe@gmail.com>
2021-10-25 17:03:38 +0000

    Add eventlistener for boxes clicked and function to compare with english word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
 let englishWords = ["dog", "cat", "fish", "giraffe", "tiger", "lion", "guinea-pig", "cheetah", "horse"];
 
 /**
+ * compare function
  * if correct - alert correct and display new english word
  * if incorrect - alert incorrect, try again and continue
  * in both cases increase number of attempts
  * alert after all nine words are found
  */
-function compare() {
 
+
+let boxes = document.getElementsByClassName("box");
+
+let compare = function() {
+    if (document.getElementById("word").innerHTML === this.getAttribute("data-vocab")) {
+        alert("correct");
+    } else {
+        alert("incorrect");
+    }
+};
+
+for (var i=0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", compare, false);
 }
 
 /**
@@ -23,5 +36,5 @@ function calculateAttempts () {
  * if clicked set number of attempts to 0
  */
 function restartGame() {
-    
+
 }
