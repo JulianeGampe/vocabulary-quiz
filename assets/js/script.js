ceb22baa7175e5304bfccc952d5086751e0835db
JulianeGampe <juliane.gampe@gmail.com>
2021-10-25 16:44:30 +0000

    Define englishWords variable and set up structure of functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log('Hello World!');
+let englishWords = ["dog", "cat", "fish", "giraffe", "tiger", "lion", "guinea-pig", "cheetah", "horse"];
+
+/**
+ * if correct - alert correct and display new english word
+ * if incorrect - alert incorrect, try again and continue
+ * in both cases increase number of attempts
+ * alert after all nine words are found
+ */
+function compare() {
+
+}
+
+/**
+ * increase attempts by one each time a germanword box is clicked
+ */
+function calculateAttempts () {
+
+}
+
+
+/**
+ * if clicked set the english word to 0
+ * if clicked set number of attempts to 0
+ */
+function restartGame() {
+    
+}
