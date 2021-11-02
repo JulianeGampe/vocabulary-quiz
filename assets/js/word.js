9b2a4b5117f2a9b2269153711636ef476826645c
JulianeGampe <juliane.gampe@gmail.com>
2021-11-02 11:00:41 +0000

    Add variables with arrays for animals, travel,food

--- assets/js/word.js
+++ assets/js/word.js
@@ -1,1 +1,115 @@
 /* jshint esversion: 8 */
+
+const animals = [{
+        "en": "dog",
+        "de": "Hund"
+    },
+    {
+        "en": "cat",
+        "de": "Katze"
+    },
+    {
+        "en": "fish",
+        "de": "Fisch"
+    },
+    {
+        "en": "giraffe",
+        "de": "Giraffe"
+    },
+    {
+        "en": "tiger",
+        "de": "Tiger"
+    },
+    {
+        "en": "lion",
+        "de": "Löwe"
+    },
+    {
+        "en": "guinea pig",
+        "de": "Meerschweinchen"
+    },
+    {
+        "en": "cheetah",
+        "de": "Gepard"
+    },
+    {
+        "en": "horse",
+        "de": "Pferd"
+    }
+];
+
+const travel = [{
+    "en": "backpack",
+    "de": "Rucksack"
+},
+{
+    "en": "suitcase",
+    "de": "Koffer"
+},
+{
+    "en": "airport",
+    "de": "Flughafen"
+},
+{
+    "en": "airplane",
+    "de": "Flugzeug"
+},
+{
+    "en": "train",
+    "de": "Zug"
+},
+{
+    "en": "main station",
+    "de": "Hauptbahnhof"
+},
+{
+    "en": "sight",
+    "de": "Sehenswürdigkeit"
+},
+{
+    "en": "beach",
+    "de": "Strand"
+},
+{
+    "en": "holiday",
+    "de": "Urlaub"
+}
+];
+
+const food = [{
+    "en": "strawberry",
+    "de": "Erdbeere"
+},
+{
+    "en": "milk",
+    "de": "Milch"
+},
+{
+    "en": "egg",
+    "de": "Ei"
+},
+{
+    "en": "bread",
+    "de": "Brot"
+},
+{
+    "en": "soup",
+    "de": "Suppe"
+},
+{
+    "en": "apple",
+    "de": "Apfel"
+},
+{
+    "en": "cookie",
+    "de": "Keks"
+},
+{
+    "en": "pineapple",
+    "de": "Ananas"
+},
+{
+    "en": "cake",
+    "de": "Kuchen"
+}
+];
